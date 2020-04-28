03e5d5e4da7b51b01aad561d252b6d46352f1d4e
Tushar-W <twankhede95@gmail.com>
2020-04-28 18:54:08 +0530

    ADDED:get three integer input and return maximum

---
+++ src/main/java/com/bl/findmaximum/FindMaximum.java
@@ -0,0 +1,15 @@
+package com.bl.findmaximum;
+
+public class FindMaximum {
+    public Integer getMaximum(Integer a, Integer b, Integer c) {
+        Integer max = a;
+        if (b.compareTo(max) > 0)
+            max=b;
+        if (c.compareTo(max) > 0)
+            max = c;
+
+        return max;
+    }
+
+
+}
