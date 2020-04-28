5334f6752c744655dc093c1438268e995803ffa5
Tushar-W <twankhede95@gmail.com>
2020-04-29 00:11:12 +0530

    MODIFIED:create one generic method instead of three method

--- src/main/java/com/bl/findmaximum/FindMaximum.java
+++ src/main/java/com/bl/findmaximum/FindMaximum.java
@@ -11,4 +11,23 @@ public class FindMaximum {
         return max;
     }
 
+   /* public Float getMaximum(Float a, Float b, Float c) {
+        Float max = a;
+        if (b.compareTo(max) > 0)
+            max=b;
+        if (c.compareTo(max) > 0)
+            max = c;
+
+        return max;
+    }
+
+    public String getMaximum(String a, String b, String c) {
+        String max = a;
+        if (b.compareTo(max) > 0)
+            max=b;
+        if (c.compareTo(max) > 0)
+            max = c;
+
+        return max;
+    }*/
 }
