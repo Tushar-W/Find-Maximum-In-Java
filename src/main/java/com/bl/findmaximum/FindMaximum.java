10cd407a597bc33999e840272931c4c5d8ba3c37
Tushar-W <twankhede95@gmail.com>
2020-04-28 22:54:15 +0530

    REFACTORED:all three method to one generic method and return maximum

--- src/main/java/com/bl/findmaximum/FindMaximum.java
+++ src/main/java/com/bl/findmaximum/FindMaximum.java
@@ -1,8 +1,8 @@
 package com.bl.findmaximum;
 
 public class FindMaximum {
-    public Integer getMaximum(Integer a, Integer b, Integer c) {
-        Integer max = a;
+    public <E extends Comparable> E getMaximum(E a, E b, E c) {
+        E max = a;
         if (b.compareTo(max) > 0)
             max=b;
         if (c.compareTo(max) > 0)
@@ -11,23 +11,4 @@ public class FindMaximum {
         return max;
     }
 
-    public Float getMaximum(Float a, Float b, Float c) {
-        Float max = a;
-        if (b.compareTo(max) > 0)
-            max=b;
-        if (c.compareTo(max) > 0)
-            max = c;
-
-        return max;
-    }
-
-    public String getMaximum(String a, String b, String c) {
-        String max = a;
-        if (b.compareTo(max) > 0)
-            max=b;
-        if (c.compareTo(max) > 0)
-            max = c;
-
-        return max;
-    }
 }
